6ef460d7ec008897fb0fa9b0a66487538891b10d
Raphael Anizio da Silva <96752946+raphaelaniziodasilva@users.noreply.github.com>
2022-08-04 20:12:45 -0300

    Tipos

--- Diferença entre Javascript para Typescript/Typescript.js
+++ Diferença entre Javascript para Typescript/Typescript.js
@@ -1,7 +1,6 @@
 "use strict";
 
-// arquivo convertido(compilado) para js
-
+// Foi gerado um arquivo um javascript pelo arquivo typescript que foi compilado
 function somar(numero1, numero2) {
     return numero1 + numero2;
 }
@@ -9,9 +8,4 @@ let firstNumber = 3;
 let secondNumber = 4;
 const results = somar(firstNumber, secondNumber);
 console.log(results);
-/* Esta escrito em TypeScript agora vou ter que converter para o Javascript ou seja vou fazer a compilação para Javascript, pois eu tenho um typescript mais preciso e do javascript para poder usar
 
-Para converter(compilar): use a linha de comando tsc
-
-Tsc: o comando tsc vai pegar o meu arquivo typescript, e vai gerar um arquivo javascript
-*/ 
---
+++ Tipos Básicos/tipos.ts
@@ -0,0 +1,59 @@
+// Para fazer as declarações de tipos no typescript primeiro você declara as variaveis e depois diz qual e o tipo delas. Os tipos sempre começam com as letra minuscula
+
+// Boolean: verdadeiro ou falso
+const contaPaga: boolean = true;
+const contaNaoPaga: boolean = false;
+
+// Number: inteiro ou real
+const idade: number = 27;
+const nota: number = 5.5;
+
+// String: caractere
+const nome: string = "Raphael Anizio";
+
+// Array só de numeros
+const loterica: number[] = [10, 44, 08, 22, 11];
+// Array de strings
+const nomes: string[] = ["anderson", "nah", "diego"];
+
+// Tuple: vai ser um array aonde pode ter Number, String e Boolean
+let jogador = [string, number, boolean];
+jogador = ["Raphael", 7, true];
+
+// Enum: é muito utilizado para fazer um mapeamento de informações 
+enum StatusAprovacao {
+    Aprovado = "001",
+    Pendente = "002",
+    Reprovado = "003"
+}
+const AprovacaoStatus: StatusAprovacao = StatusAprovacao.Pendente;
+
+// Any: aceita qualquer coisa, quando for utilizar e para utilizar com muita cautela porque e realmente quando voce não precisa saber qual e o tipo
+const retornoApi: any[] = [12, "Cobertor", false];
+
+const retornoApi2: any = {
+    // aqui eu vou receber varias coisas que eu não sei o que e 
+}
+
+// Void: e utilizado em funções que e para dizer quando uma função não retorna nada
+function printarNaTela (msg: string): void {
+    console.log(msg);
+
+}
+
+// Null e Undefined:
+const u: undefined = undefined;
+const n: null = null;
+
+// Object:
+function criar(object: object) {
+    // ...
+}
+criar({
+    propriedade: 1
+
+})
+// criar("raphael") // da erro
+
+
+
